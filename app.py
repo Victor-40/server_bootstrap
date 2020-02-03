4b601dba9a1231ba24bfbb2e2d66c35482ac29b0
Victor Cherezov <victor_cherezov@mentor.com>
2020-02-03 18:39:44 +0300

    wip

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 import re
 import vix
 import sys
+from win32com import client
+import pythoncom
 
 # configuration
 DEBUG = True
@@ -52,6 +54,49 @@ def find_builds(build, tag, _prod, subdir):
                 if re.search(patt, item.name):
                     setups.append(item.path)
     return setups
+
+
+def make_xls(setups):
+    result = list()
+    with open(r'c:\exp_vue_bootstrap\server\cfg.json') as fi:
+        cfg_dct = json.load(fi)
+
+    with open(r'c:\exp_vue_bootstrap\server\snap_dct.json') as fi:
+        vms = json.load(fi)
+
+    for _setup in setups:
+        setup_prefix = os.path.basename(_setup).split('-')[0]
+        snapshot_prefix = cfg_dct['prod_snaps'][setup_prefix]
+        for _vm in vms:
+            vm_name = _vm.replace('.vmx', '')
+            vm_path = vms[_vm]['pth']
+            for k in vms[_vm]['sn']:
+                if k.startswith(snapshot_prefix):
+                    result.append((_setup,  vm_name, vm_path,  k, "0"))
+
+    job_file = r'd:\Testing\VMWare\VM-Monitor-test.Jobs.xls'
+    pythoncom.CoInitialize()
+    xls = client.Dispatch("Excel.Application")
+
+    wb = xls.Workbooks.Add()
+    sheet = wb.WorkSheets("Sheet1")
+    sheet.Name = "Table"
+
+    # header
+    header_list = ["InstallPath", "Name", "Path", "SnapName", "Done"]
+    for i in range(len(header_list)):
+        sheet.Cells(1, i + 1).value = header_list[i]
+
+    for i in range(len(result)):
+        for j in range(5):
+            sheet.Cells(i + 2, j + 1).value = result[i][j]
+
+    wb.SaveAs(job_file, 56)
+    wb.Close()
+    pythoncom.CoUninitialize()
+
+    return result
+
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
